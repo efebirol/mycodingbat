9740749ce97f40175698751562fd59cf55058e89
Your Name <you@example.com>
2019-07-04 17:28:59 +0200

    teste die Lombok Getter und Setter Funktionalität

--- src/main/java/com/codingchallenge/codingchallgenge/controller/UserController.java
+++ src/main/java/com/codingchallenge/codingchallgenge/controller/UserController.java
@@ -6,25 +6,37 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.codingchallenge.codingchallgenge.webservice.UserService;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import org.apache.catalina.User;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/testresults")
+@Getter @Setter // <--- Benutze Lombok Bibliothek (für Getter und Setter)
 public class UserController {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 	private final UserService userService;
+	private String name;
 
 	@Autowired
 	public UserController(UserService userService) {
 
-		System.out.println("-- Usercontroller.java - Usercontroller() Konstruktor");
+		logger.info("-- Usercontroller.java - Usercontroller() Konstruktor");
 		this.userService = userService;
+		this.setName("TestLombokGetterName");
 	}
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String getUserWebpage() {
-		System.out.println("-- Usercontroller.java - getUserWebpage()");
-		return "testresultpage";
+		logger.info("-- Usercontroller.java - getUserWebpage()");
+		return this.getName();
 	}
+	
+	
 
 }
