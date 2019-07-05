112fd7f58afd05d343ddb28adeb64fb27d54de88
Your Name <you@example.com>
2019-07-05 19:12:31 +0200

    schreibe test mit stringe

--- src/main/java/com/codingchallenge/controller/UserController.java
+++ src/main/java/com/codingchallenge/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.codingchallenge.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,16 +11,19 @@ import com.codingchallenge.webservice.UserService;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.websocket.server.PathParam;
+
 import org.apache.catalina.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/testresults")
-@Getter @Setter // <--- Benutze Lombok Bibliothek (für Getter und Setter)
+@Getter
+@Setter // <--- Benutze Lombok Bibliothek (für Getter und Setter)
 public class UserController {
 
-    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 	private final UserService userService;
 	private String name;
 
@@ -31,11 +35,27 @@ public class UserController {
 		this.setName("TestLombokGetterName");
 	}
 
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(value = "/getuserwebpage", method = RequestMethod.GET)
 	public String getUserWebpage() {
 		logger.info("-- Usercontroller.java - getUserWebpage()");
 		return this.getName();
 	}
-	
+
+	/**
+	 * Test in Konsole mit "curl
+	 * http://localhost:8080/testresults/stringe/IrgendeinString"
+	 */
+	@RequestMapping(value = "/stringe/{inputstring}", method = RequestMethod.GET)
+	public boolean stringE(@PathVariable("inputstring") String inputstring) {
+		int count = 0;
+		logger.info("-- UserController -- stringE() - inputstring: " + inputstring);
+
+		for (int i = 0; i < inputstring.length(); i++) {
+			if (inputstring.charAt(i) == 'e')
+				count++;
+		}
+
+		return (count >= 1 && count <= 3) ? true : false;
+	}
 
 }
--- src/test/java/com/codingchallenge/controller/UserControllerTest.java
+++ src/test/java/com/codingchallenge/controller/UserControllerTest.java
@@ -29,22 +29,35 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(UserController.class)
 public class UserControllerTest {
 
-    @MockBean
-    private UserService userService;
-    @MockBean
-    private UserController userController;
-    @Autowired
-    private MockMvc mockMvc;	
-	
-	
+	@MockBean
+	private UserService userService;
+	@MockBean
+	private UserController userController;
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	public void testGetUserWebpage() throws Exception
-	{
+	public void testGetUserWebpage() throws Exception {
 		String shouldTestname = "TestLombokGetterName";
-		//given(reservationService.getRoomReservationsForDate("2017-01-01")).willReturn(mockReservationList);
-        //this.mockMvc.perform(get("/testresults")).andExpect(status().isOk()).andExpect(content().string(containsString("Test, JUnit")));
+		// given(reservationService.getRoomReservationsForDate("2017-01-01")).willReturn(mockReservationList);
+		// this.mockMvc.perform(get("/testresults")).andExpect(status().isOk()).andExpect(content().string(containsString("Test,
+		// JUnit")));
 		given(userController.getUserWebpage()).willReturn(shouldTestname);
-		this.mockMvc.perform(get("/testresults")).andExpect(status().isOk());
+		this.mockMvc.perform(get("/testresults/getuserwebpage")).andExpect(status().isOk());
+	}
+
+	/**
+	 * Return true if the given string contains between 1 and 3 'e' chars.
+	 * Testfälle: stringE("Hello") → true stringE("Heelle") → true
+	 * stringE("Heelele") → false
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testStringE() throws Exception {
+		String testHello = "Hello";
+		this.mockMvc.perform(get("/testresults/stringe/{inputstring}","Hello")).andExpect(status().isOk())
+		//.andExpect(content().string("true"));
 	}
 
 }
