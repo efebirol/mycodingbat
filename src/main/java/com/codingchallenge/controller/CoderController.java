cb7eb4cefe12c8e94d819c137490d76398c22192
Your Name <you@example.com>
2019-10-30 10:14:43 +0100

    Error handling fuer allgemeinen Fehler, 404 und 500 über Thymeleaf

--- src/main/java/com/codingchallenge/controller/CoderController.java
+++ src/main/java/com/codingchallenge/controller/CoderController.java
@@ -311,16 +311,31 @@ public class CoderController
   public int arrayCount9(@PathVariable("nums") int[] nums)
   {
     int result = 0;
-//    LOGGER.info("\"-- CoderController.java - arrayCount9()");
+    // LOGGER.info("\"-- CoderController.java - arrayCount9()");
 
     for ( int i = 0 ; i < nums.length ; i++ )
     {
-//      LOGGER.info("\"-- CoderController.java - arrayCount9() - nums:" + nums[i]);
+      // LOGGER.info("\"-- CoderController.java - arrayCount9() - nums:" + nums[i]);
       result = (nums[i] == 9) ? (result + 1) : result;
     }
 
     return result;
   }
 
+
+  /**
+   * Given an array of ints, return true if one of the first 4 elements in the array is a 9. The array length may be less than 4.
+   * arrayFront9([1, 2, 9, 3, 4]) → true
+   * arrayFront9([1, 2, 3, 4, 9]) → false
+   * arrayFront9([1, 2, 3, 4, 5]) → false
+   */
+  @RequestMapping(value = "/arrayFront9/{nums}", method = RequestMethod.GET)
+  public boolean arrayFront9(int[] nums)
+  {
+
+    return true;
+
+
+  }
 
 }
